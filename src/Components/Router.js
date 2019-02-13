160796b001ad0f38166bb58d73d024a1b7ebf98b
philastrio <han_gang@naer.com>
2019-02-14 06:20:50 +0900

    Router 1차 작업

---
+++ src/Components/Router.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Redirect, Switch} from "react-router-dom";
+import Home from 'Routes/Home';
+import TV from 'Routes/TV';
+import Search from 'Routes/Search';
+
+export default () => (
+	<Router>
+		<Switch>
+			<Route path="/" exact component={Home} />
+			<Route path="/tv" exact component={TV} />
+			<Route path="/tv/popular" render={() => <h1>Popular</h1>}/>
+			<Route path="/search" exact component={Search} />
+			<Redirect from='*' to='/' />
+		</Switch>
+	</Router>
+);
