a05d094aa91f63e8e4598c7a4a8d0eb15f42de38
philastrio <han_gang@naer.com>
2019-02-24 14:29:04 +0900

    1차 build

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Redirect, Switch} from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
 import Home from 'Routes/Home';
 import TV from 'Routes/TV';
 import Search from 'Routes/Search';
@@ -7,17 +12,35 @@ import Detail from 'Routes/Detail';
 import Header from 'Components/Header';
 
 export default () => (
-	<Router>
-		<>
-			<Header/>
-			<Switch>
-				<Route path="/" exact component={Home} />
-				<Route path="/tv" exact component={TV} />
-				<Route path="/search" exact component={Search} />
-				<Route path="/movie/:id" component={Detail} />
-				<Route path="/show/:id" component={Detail} />
-				<Redirect from='*' to='/' />
-			</Switch>
-		</>
-	</Router>
+  <Router>
+    <>
+      <Header />
+      <Switch>
+        <Route
+          path="https://philastrio.github.io/hkflix/"
+          exact
+          component={Home}
+        />
+        <Route
+          path="https://philastrio.github.io/hkflix/tv"
+          exact
+          component={TV}
+        />
+        <Route
+          path="https://philastrio.github.io/hkflix/search"
+          exact
+          component={Search}
+        />
+        <Route
+          path="https://philastrio.github.io/hkflix/movie/:id"
+          component={Detail}
+        />
+        <Route
+          path="https://philastrio.github.io/hkflix/show/:id"
+          component={Detail}
+        />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </>
+  </Router>
 );
